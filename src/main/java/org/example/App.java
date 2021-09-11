43bf8181aa311eb6d10be5891569b54e23947694
Thach Vo <thachvo530@yahoo.com>
2021-09-11 14:13:43 -0400

    update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,33 +8,33 @@ package org.example;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class App
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
 
         String WI = "WI";
         DecimalFormat df = new DecimalFormat("0.00");
 
         System.out.print("What is the order amount? ");
         Scanner amount = new Scanner(System.in);
-        int amount1 = amount.nextInt();
+        double amount1 = amount.nextDouble();
 
         System.out.print("What is the State? ");
         Scanner state = new Scanner(System.in);
         String state1 = state.next();
 
-        if(state1 == WI) {
+        if (state1.equals(WI)) {
 
             double tax = 0.055 * amount1;
             double total = amount1 + tax;
 
-            System.out.print("The subtotal is $"+df.format(amount1)+".\nThe tax is $"+df.format(tax)+".\nThe total is $"+df.format(total)+".");
+            System.out.print("The subtotal is $" + df.format(amount1) + ".\nThe tax is $" + df.format(tax) + ".\nThe total is $" + df.format(total) + ".");
 
         }
 
-        if(state1 != WI)
+        if (!state1.equals(WI)) {
 
-            System.out.print("The total is $"+df.format(amount1)+".");
+            System.out.print("The total is $" + df.format(amount1) + ".");
 
+        }
     }
 }

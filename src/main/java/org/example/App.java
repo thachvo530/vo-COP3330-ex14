e208aaf9a2d8183b04b8affc72386f4451953b10
Thach Vo <thachvo530@yahoo.com>
2021-09-11 14:08:29 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Thach Vo
+ */
+
+package org.example;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args ) {
+
+        String WI = "WI";
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.print("What is the order amount? ");
+        Scanner amount = new Scanner(System.in);
+        int amount1 = amount.nextInt();
+
+        System.out.print("What is the State? ");
+        Scanner state = new Scanner(System.in);
+        String state1 = state.next();
+
+        if(state1 == WI) {
+
+            double tax = 0.055 * amount1;
+            double total = amount1 + tax;
+
+            System.out.print("The subtotal is $"+df.format(amount1)+".\nThe tax is $"+df.format(tax)+".\nThe total is $"+df.format(total)+".");
+
+        }
+
+        if(state1 != WI)
+
+            System.out.print("The total is $"+df.format(amount1)+".");
+
+    }
+}
